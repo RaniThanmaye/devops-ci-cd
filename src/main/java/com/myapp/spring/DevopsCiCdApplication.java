ecfffa6134be515e13c5bf835fb460edd5160231
RaniThanmaye <ranisudharshan24@gmail.com>
2022-02-03 11:01:50 +0530

    feature updated

--- src/main/java/com/myapp/spring/DevopsCiCdApplication.java
+++ src/main/java/com/myapp/spring/DevopsCiCdApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevopsCiCdApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hii");
 		SpringApplication.run(DevopsCiCdApplication.class, args);
 	}
 
